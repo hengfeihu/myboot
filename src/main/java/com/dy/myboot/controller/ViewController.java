6c6e661eeb55514d54e1ad1ec06a8c301e99b5d7
hengfeihu <137071653@qq.com>
2018-06-14 15:45:21 +0800

    mybatis-generator 修改不覆盖之前的xml文件

--- src/main/java/com/dy/myboot/controller/ViewController.java
+++ src/main/java/com/dy/myboot/controller/ViewController.java
@@ -3,8 +3,12 @@ package com.dy.myboot.controller;
 import com.dy.myboot.dao.UserMapper;
 import com.dy.myboot.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,19 +16,50 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ViewController {
 
     @Autowired
+    private DataSourceTransactionManager transactionManager;
+
+    @Autowired
     private UserMapper userMapper;
+
+    @ResponseBody
+    @GetMapping("/test1")
+    public String testTransaction1() {
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(def);
+        User user = null;
+        try {
+            user = new User();
+            user.setUsername("xx");
+            user.setPassword("ppp");
+            user.setPhone("2321312");
+            userMapper.insert(user);
+            String a = null;
+            a.indexOf("b");
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            transactionManager.rollback(status);
+        }
+        return user.toString();
+    }
 
     @Transactional
     @ResponseBody
-    @GetMapping("/")
-    public String index() {
-        User user = new User();
-        user.setUsername("xx");
-        user.setPassword("ppp");
-        user.setPhone("2321312");
-        userMapper.insert(user);
-        String a = null;
-        a.indexOf("b");
+    @GetMapping("/test2")
+    public String testTransaction2() {
+        User user = null;
+        try {
+            user = new User();
+            user.setUsername("xx");
+            user.setPassword("ppp");
+            user.setPhone("2321312");
+            userMapper.insert(user);
+            String a = null;
+            a.indexOf("b");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return user.toString();
     }
 }
