b346e85945eb2eaacf9cf02832ce2331f9e32a96
hengfeihu <137071653@qq.com>
2018-06-15 15:35:34 +0800

    分页测试

--- src/main/java/com/dy/myboot/controller/UserController.java
+++ src/main/java/com/dy/myboot/controller/UserController.java
@@ -2,9 +2,12 @@ package com.dy.myboot.controller;
 
 import com.dy.myboot.dao.UserMapper;
 import com.dy.myboot.model.User;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +19,10 @@ public class UserController {
     private UserMapper userMapper;
 
     @RequestMapping(value = "all", method = RequestMethod.GET)
-    public List<User> all() {
-        return userMapper.selectAll();
+    public PageInfo<User> all(@RequestParam(value = "pageNum", required = false, defaultValue = "1") int pageNum,
+                              @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<User> userDomains = userMapper.selectAll();
+        return new PageInfo<>(userDomains);
     }
 }
